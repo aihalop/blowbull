f3201da6a68f1129d147e7d89a45e36be59ca50e
Cao Jin <aihalop@gmail.com>
2017-09-18 21:53:31 +0800

    add sperated figures for pdf and cdf

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ def binomial(k, n, p):
     return combination(k, n) * (p**k) * ((1 - p)**(n - k))
 
 
-def distribution(probabilities):
+def cdf(probabilities):
     '''
     '''
     return [sum(probabilities[:k]) for k in range(len(probabilities))]
@@ -47,15 +47,25 @@ def probability_statement(k, n, m, t, p=1/6):
 
 if __name__=="__main__":
     import matplotlib.pyplot as plt
-    n = 30
+    n = 45
     t = 6
     m = 3
     index = range(t, n + 1)
     probs_without_posterior = [binomial(k, n, 1/6) for k in index]
     probs_with_posterior = [probability_statement(k, n, m, t) for k in index]
     
+    plt.figure()
+    plt.plot(index, cdf(probs_without_posterior), '-ro', label="totally unknown")
+    plt.plot(index, cdf(probs_with_posterior), '-go', label='known {m}'.format(m=m))
+    plt.legend()
+    plt.title("the cdf of the distribution of the number of same number, n = {}, t = {}, m = {}".format(n, t, m))
+    plt.grid()
 
-    plt.plot(index, distribution(probs_without_posterior), '-ro')
-    plt.plot(index, distribution(probs_with_posterior), '-go')
-    plt.legend(['totally unknown', 'known {m}'.format(m=m)])
+    plt.figure()
+    plt.plot(index, probs_without_posterior, '-ro', label="totally unknown")
+    plt.plot(index, probs_with_posterior, '-go', label='known {m}'.format(m=m))
+    plt.legend()
+    plt.title("the probability of the number of same number, n = {}, t = {}, m = {}".format(n, t, m))
+    plt.grid()
+
     plt.show()
